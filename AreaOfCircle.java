8f6c99c6051648850b1449eb2c40c7e60d0b8e0c
SushantPatil77 <91300978+SushantPatil77@users.noreply.github.com>
2021-09-27 15:45:07 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		//3) Write a program to print the area and perimeter of acircle.
+
+         int r=18;
+         
+       double area=Math.PI*r;
+       System.out.println("Area="+area);
+       
+       double perimeter=2*(Math.PI*r);
+       System.out.println("Perimeter"+perimeter);
+         
+         
+	}
+
+}
